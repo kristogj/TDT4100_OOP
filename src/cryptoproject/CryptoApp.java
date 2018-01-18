9aff9386a7826fb3b98f9b5ccf683b5e4afecc44
kristogj <kristoffergjerde@gmail.com>
2018-01-18 17:19:55 +0100

    ..

---
+++ src/cryptoproject/CryptoApp.java
@@ -0,0 +1,25 @@
+package cryptoproject;
+
+
+
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CryptoApp extends Application {
+
+	 @Override
+	 public void start(Stage stage) throws Exception {
+	    Parent panel = FXMLLoader.load(getClass().getResource("Crypto.fxml"));
+	    Scene scene = new Scene(panel);
+	    stage.setScene(scene);
+	    stage.show();
+	  }
+	
+	  public static void main(String[] args) {
+	    launch(args);
+	  }
+}
--- src/cryptoproject/CryptoController.java
+++ src/cryptoproject/CryptoController.java
@@ -1,6 +1,7 @@
 package cryptoproject;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -11,55 +12,53 @@ import javafx.scene.control.TextField;
 
 public class CryptoController {
 	
-	Crypto crypto = new Crypto();
+	Crypto crypto;
+	DecimalFormat numFormat = new DecimalFormat("#.00");
 	
 	static String BTC = "https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=NOK";
 	static String LTC = "https://api.coinmarketcap.com/v1/ticker/litecoin/?convert=NOK";
 	static String ETH = "https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=NOK";
 	
-	ArrayList<String> coins = new ArrayList<String>(Arrays.asList(BTC,LTC,ETH));
+	ArrayList<String> api;
 	
 	@FXML
 	Button updateButton;
 	
 	@FXML
-	Label btcusd, ltcusd, ethusd, btcnok, ltcnok, ethnok;
+	Label btcusd, btcnok, ltcusd, ltcnok, ethusd, ethnok;
 	
 	@FXML
-	Label btcnokWallet, ethnokWallet, ltcnokWallet;
+	Label btcusdWallet, btcnokWallet, ltcusdWallet, ltcnokWallet, ethusdWallet, ethnokWallet;
 	
 	@FXML
-	TextField btcfield, ethfield, ltcfield;
+	TextField btcfield, ltcfield, ethfield;
 	
 	
 	public CryptoController() {
+		this.crypto = new Crypto();
+		this.api = new ArrayList<String>(Arrays.asList(BTC,LTC,ETH));
 		
 	}
+
 	
 	public void update() throws IOException{
-		ArrayList<Double> btc = crypto.URLReader(BTC);
-		ArrayList<Double> ltc = crypto.URLReader(LTC);
-		ArrayList<Double> eth = crypto.URLReader(ETH);
+		ArrayList<Label> tickers = new ArrayList<Label>(Arrays.asList(btcusd, btcnok, ltcusd, ltcnok, ethusd, ethnok));
+		ArrayList<Label> wallets = new ArrayList<Label>(Arrays.asList(btcusdWallet, btcnokWallet, ltcusdWallet, ltcnokWallet, ethusdWallet, ethnokWallet));
+		ArrayList<TextField> walletFields = new ArrayList<TextField>(Arrays.asList(btcfield, ltcfield, ethfield));
 		
-		btcusd.setText(Double.toString(btc.get(0)));
-		ethusd.setText(Double.toString(eth.get(0)));
-		ltcusd.setText(Double.toString(ltc.get(0)));
-		btcnok.setText(Double.toString(btc.get(1)));
-		ethnok.setText(Double.toString(eth.get(1)));
-		ltcnok.setText(Double.toString(ltc.get(1)));
+		for (int x = 0; x < this.api.size();x++) {
+			ArrayList<Double> values = this.crypto.URLReader(this.api.get(x));
+			//Update Tickers
+			tickers.get(2*x).setText(numFormat.format(values.get(0)));
+			tickers.get(2*x+1).setText(numFormat.format(values.get(1)));
+			
+			//Update wallets
+			wallets.get(2*x).setText(numFormat.format(values.get(0)*Double.parseDouble(walletFields.get(x).getText())));
+			wallets.get(2*x+1).setText(numFormat.format(values.get(1)*Double.parseDouble(walletFields.get(x).getText())));
+		}
 		
 	}
 	
-	public void updateWallet() {
-		btcnokWallet.setText(Double.toString(Double.parseDouble(btcfield.getText())
-				*Double.parseDouble(this.btcnok.getText())));
-		
-		ethnokWallet.setText(Double.toString(Double.parseDouble(ethfield.getText())
-				*Double.parseDouble(this.ethnok.getText())));
-		
-		ltcnokWallet.setText(Double.toString(Double.parseDouble(ltcfield.getText())
-				*Double.parseDouble(this.ltcnok.getText())));
-	}
 	
 	
 }
