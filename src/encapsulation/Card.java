e688d4bcbeace62fa195082bc18d8d9f63bab6e8
kristogj <kristoffergjerde@gmail.com>
2018-01-29 23:37:30 +0100

    ..

---
+++ src/encapsulation/Card.java
@@ -0,0 +1,33 @@
+package encapsulation;
+
+public class Card {
+	
+	static String validSuit = "SHDC";
+	
+	private char suit;
+	private int face;
+	
+	public Card(char suit, int face) {
+		if(validSuit.indexOf(suit) == -1 || face < 1 || face > 13) {
+			throw new IllegalArgumentException("Unvalid d");
+		}
+		this.suit = suit;
+		this.face = face;
+	}
+	
+	public char getSuit() {
+		return this.suit;
+	}
+	
+	public int getFace() {
+		return this.face;
+	}
+	
+	@Override
+	public String toString() {
+		return getSuit() + "" + getFace();
+	}
+	
+
+	
+}
